0023dfcf978bc91da81f5f928eb95390be210e1c
carlosordaz <cordaz@corsusa.com>
2026-03-02 15:25:59 -0500

    cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const httpServer = createServer(app)
 
 const io = new Server(httpServer, {
     cors: {
-        origin: ['http://localhost:5173', 'http://localhost:4173'],
+        origin: ['http://localhost:5173', 'http://localhost:4173', 'https://games-bay-rho.vercel.app'],
         methods: ['GET', 'POST']
     }
 })
